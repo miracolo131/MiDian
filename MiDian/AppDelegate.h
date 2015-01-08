32e36ed10cd1bbce9b26b9fb6768c5169ab6303d
miracolo131 <miracolo0131@gmail.com>
2015-01-08 14:24:58 +0800

    Initial Commit

---
+++ MiDian/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MiDian
+//
+//  Created by Miracolo Bosco on 15/1/8.
+//  Copyright (c) 2015年 MI-31. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MiDian/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MiDian
+//
+//  Created by Miracolo Bosco on 15/1/8.
+//  Copyright (c) 2015年 MI-31. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
